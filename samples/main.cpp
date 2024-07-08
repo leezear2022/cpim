50f2ad075634bcdfda8aba0de9008dfcd49141ae
leezear <leezear@gmail.com>
2024-07-08 17:36:52 +0800

    完成HModel

--- samples/main.cpp
+++ samples/main.cpp
@@ -3,16 +3,19 @@
 //
 #include <iostream>
 
-#include "xcsp3model/h_model.h"
+#include "xcsp3model/HModel.h"
 using namespace cpim::common;
 
-int main(int argc, char *argv[]) { {
-        auto var0 = HVarNode::Make(0, "var0", 0, 10);
-        auto var1 = HVarNode::Make(1, "var1", 0, 10);
+int main(int argc, char* argv[])
+{
+    {
+        auto var0 = HVarNode::Make(0, 0, "var0", 0, 10);
+        auto var1 = HVarNode::Make(1, 1, "var1", 0, 10);
+        var0->Show();
         std::cout << var0->id << std::endl;
         std::cout << var1->id << std::endl;
         // std::vector<<>>
-        std::vector<std::vector<int> > myVector = {{0, 0}, {1, 1}};
+        std::vector<std::vector<int>> myVector = {{0, 0}, {1, 1}};
         std::vector<HVar> scp = {var0, var1};
         auto tab = HTabNode::Make(0, true, myVector, scp);
         std::cout << tab->id << std::endl;
---
+++ xcsp3parser/cmake-build-release/CMakeFiles/samplesXcsp3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../samples/samplesXcsp3"
+  "../samples/samplesXcsp3.pdb"
+  "CMakeFiles/samplesXcsp3.dir/samples/main.cc.o"
+  "CMakeFiles/samplesXcsp3.dir/samples/main.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/samplesXcsp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
